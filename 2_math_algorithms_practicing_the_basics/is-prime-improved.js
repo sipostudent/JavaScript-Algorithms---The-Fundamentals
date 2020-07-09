ef47784dc1b376fc711cc3b9153996564dd36e22
Sipo Charles <sipocharles@yahoo.co.uk>
2020-07-10 00:23:19 +0100

    Is Prime - Improved

---
+++ 2_math_algorithms_practicing_the_basics/is-prime-improved.js
@@ -0,0 +1,24 @@
+/**
+ * The Primality Test
+ */
+
+// "Is Prime", Big O & Multiple Cases
+
+// Solution
+function isPrime(number) {
+    for (let i = 2; i < Math.sqrt (number); i++) { // 1
+      console.log('Running');
+      if (number % i === 0) { // n
+        return false; // 1
+      } 
+    }
+    return true; // 1
+  }
+  
+  // Best Case: number  = 1 OR number = 2 => 0(1)
+  // Average Case: 0(n)
+  // Worst Case: number = 27,227 => 0(n) (Improved: 0(sqrt(n)))
+  
+  console.log(isPrime(27277));
+  // console.log(isPrime(5));
+  // console.log(isPrime(9));
