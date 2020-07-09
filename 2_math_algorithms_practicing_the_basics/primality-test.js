30ab9b01470a2705fe5af77da1c4c04c79a68e1f
Sipo Charles <sipocharles@yahoo.co.uk>
2020-07-09 20:55:53 +0100

    The Primality Test

---
+++ 2_math_algorithms_practicing_the_basics/primality-test.js
@@ -0,0 +1,16 @@
+/**
+ * The Primality Test
+ */
+
+// Solution
+function isPrime(number) {
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) {
+      return false;
+    } 
+  }
+  return true;
+}
+
+console.log(isPrime(5));
+console.log(isPrime(9));
